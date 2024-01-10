bf042b426762b746a0e802bcab1ebf44e18f4fe0
NashigiTV <123368596+NashigiTV@users.noreply.github.com>
2024-01-10 10:37:50 +0100

    Amélioration Scène ChoixSkin

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CharacterManager : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class CharacterManager : MonoBehaviour
     public PlayerData playerData;
 
     public Image imagePerso;
+    public TMP_Text nameText;
 
     public CharacterDatabase dbPlayer;
     private int selectedOption = 0;
@@ -67,5 +69,6 @@ public class CharacterManager : MonoBehaviour
         Character character = dbPlayer.GetCharacter(selectedOption);
         artworkSprite.sprite = character.characterSprite;
         imagePerso.sprite = character.characterSprite;
+        nameText.text = character.characterName;
     }
 }
