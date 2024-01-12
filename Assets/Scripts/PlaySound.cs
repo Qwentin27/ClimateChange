75cd6265391ab7decd9e85d282b5737e82cba92f
NashigiTV <123368596+NashigiTV@users.noreply.github.com>
2024-01-12 19:15:53 +0100

    SoundDesign SceneBuilder

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource soundPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void playThisSoundEffect()
+    {
+        soundPlayer.Play();
+    }
+}
